4af75e57daafa3cfc7e8af808ccaaf47e0b5a0d6
andreh0v <fredrikandrebertelsen@gmail.com>
2026-06-04 16:23:32 +0200

    Task 1 complete - print_stock_info_workin. Learning more about parameteres

---
+++ main.py
@@ -0,0 +1,66 @@
+#Building a portoflio tracker for a private investor. It is tracking positions, calculating retruns and getting the full portfolio overview.
+# These task was created with claude,
+from datetime import datetime
+class stock:
+    def __init__(self, ticker,name, shares,currency, buy_price, current_price, buy_year, buy_month):
+        self.ticker = ticker
+        self.name = name
+        self.shares = shares
+        self.currency = currency
+        self.buy_price = buy_price
+        self.current_price = current_price
+        self.buy_year = buy_year
+        self.buy_month = buy_month
+    def print_stock_info(self):pass
+portfolio = {
+    "AAPL": {
+        "ticker": "AAPL",
+        "name": "Apple Inc.",
+        "shares": 10,
+        "currency":"USD",
+        "buy_price": 150.0,
+        "current_price": 192.5,
+        "buy_year": 2023,
+        "buy_month": 1
+    },
+    "EQNR": {
+        "ticker": "EQNR",
+        "name": "Equinor ASA",
+        "shares": 25,
+        "currency":"NOK",
+        "buy_price": 310.0,
+        "current_price": 278.4,
+        "buy_year": 2022,
+        "buy_month": 6
+    },
+    "MSFT": {
+        "ticker": "MSFT",
+        "name": "Microsoft Corp.",
+        "shares": 5,
+        "currency":"USD",
+        "buy_price": 280.0,
+        "current_price": 415.0,
+        "buy_year": 2021,
+        "buy_month": 11
+    }
+}
+
+test_stock = portfolio["AAPL"]
+#Month converter to get a written output
+months = {
+    1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September",
+    10 : "October", 11: "November", 12: "December"}
+
+
+
+#Task 1 - simply printing stock info
+
+def print_stock_info( stock):
+    print(f"Ticker: {stock['ticker']}")
+    print(f"Name: {stock['name']}")
+    print(f"Shares in portfolio: {stock['shares']}")
+    print(f"Buy price: {stock['buy_price']} {stock['currency']} per share")
+    print(f"Current stock price {stock['current_price']}  {stock['currency']} per share")
+    print (f" Bought {months[stock['buy_month']]} / {stock['buy_year']}")
+
+print_stock_info(test_stock)

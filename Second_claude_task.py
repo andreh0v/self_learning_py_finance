35f9e5c6da161bf1023e510b70eda2441c22d33e
andreh0v <fredrikandrebertelsen@gmail.com>
2026-06-09 15:31:15 +0200

    task 6 fixing for currency, and adding a pct variable for the whole portfolio

--- Second_claude_task.py
+++ Second_claude_task.py
@@ -36,3 +36,16 @@ if __name__ == "__main__":
     print(winner[['ticker', 'total_return']])
     print("\nNOK holdings:")
     print(NOK_STOCK[['ticker', 'currency', 'current_price', 'pct_return']])
+    #Task 5 Summary statistics. This is for the built in functions for created calculations in task 1.
+    total_value = (df['current_price'] * df['shares']).sum()
+    average_return_pct = df['pct_return'].mean()
+    best = df.loc[df['pct_return'].idxmax()]
+    print(f"Best performer: {best['ticker']} with {best['pct_return']:.2f}%")
+    #Task 6 Fixing for currency
+    df['current_price_usd'] = df.apply(value_in_usd, axis=1, price_column='current_price')
+    df['buy_price_usd'] = df.apply(value_in_usd, axis=1, price_column='buy_price')
+    df['total_return_usd'] = (df['current_price_usd'] - df['buy_price_usd']).round(2)
+    total_pct = df['total_return_usd'].sum() / df['buy_price_usd'].sum() * 100 #Has to be a variable since it is for the entire portofolio
+    print(f"Total portfolio value : {df['current_price_usd'].sum():.2f} USD")
+    print(f"Total portfolio return: {df['total_return_usd'].sum():.2f} USD")
+    print(f"Total portfolio % return: {total_pct:.2f} %")

2d77601c8eb531d3652d395532f530575050d556
andreh0v <fredrikandrebertelsen@gmail.com>
2026-07-22 12:34:34 +0200

    improvements & bug fixes

--- NHH_assignments/BAN401_midterm/Midterms.py
+++ NHH_assignments/BAN401_midterm/Midterms.py
@@ -17,33 +17,37 @@ def problem1_trip():
 #Problem 2
 def problem2_trip():
     total_hours = 0
-    no_command = ["NO","no", "N", "n", "Nei", "NEI" ]
-    intensity_cals = {"easy" : 300, "medium": 450, "hard": 600}
-    total_hours = int(input("How many hours did you walk up Ulriken? "))
+    intense_hours = 0
+    no_command = ["NO", "no", "N", "n", "Nei", "NEI"]
+    yes_command = ["YES", "yes", "Y", "Yes", "y", "ja", "JA", "J", "j"]
+    intensity_cals = {"easy": 300, "medium": 450, "hard": 600}
+    total_hours = float(input("How many hours did you walk up Ulriken? "))
     if total_hours > 0:
         Yes_no = input("Where they all the same insensity?")
-        if Yes_no in no_command:
-            while True:
-                easy_hours = int(input("How many of the hours where a easy intensity?"))
-                normal_hours = int(input("How many of the hours where normal intensity?"))
-                hard_hours = int(input("How many of the hours where a hard intensity?"))
-                if easy_hours + normal_hours + hard_hours < 0:
-                    print("cant walk negative hours!")
-                    continue
-                elif easy_hours + normal_hours + hard_hours == 0:
-                   print("You did not walk Ulriken!")
-                   continue
-                elif easy_hours + normal_hours + hard_hours != total_hours:
-                    print(f"You said you walked {total_hours} hours!")
-                    continue
-                else:
-                    total_cals_burned=easy_hours * intensity_cals["easy"] + normal_hours * intensity_cals["medium"] + hard_hours * intensity_cals["hard"]
-                    print(f"Calories burned {total_cals_burned} calories.")
-                    break
-        else:
-            intensity = input("What was the intensity (easy/medium/hard): ?")
-            print(f"calories burned {intensity_cals[intensity] * total_hours}kcal")
-
+        while True:
+            if Yes_no in no_command:
+                while True:
+                    easy_hours = float(input("How many of the hours where a easy intensity?"))
+                    normal_hours = float(input("How many of the hours where normal intensity?"))
+                    hard_hours = float(input("How many of the hours where a hard intensity?"))
+                    if easy_hours + normal_hours + hard_hours < 0:
+                        print("cant walk negative hours!")
+                        continue
+                    elif round(easy_hours + normal_hours + hard_hours, 2) == 0:
+                        print("You did not walk Ulriken!")
+                        continue
+                    elif round(easy_hours + normal_hours + hard_hours, 2) != round(total_hours, 2):
+                        print(f"You said you walked {total_hours} hours!")
+                        continue
+                    else:
+                        total_cals_burned = easy_hours * intensity_cals["easy"] + normal_hours * intensity_cals[
+                            "medium"] + hard_hours * intensity_cals["hard"]
+                        print(f"Calories burned {total_cals_burned} calories.")
+                        break
+                break
+            else:
+                print("type intensity!")
+                continue
 
     else:
         print("You did not walk Ulriken! You should try it!")

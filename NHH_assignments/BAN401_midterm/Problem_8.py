076893bae6a3294ec621d3e8f9155e411093e33b
andreh0v <fredrikandrebertelsen@gmail.com>
2026-07-26 19:47:37 +0200

    problem 8

--- NHH_assignments/BAN401_midterm/Problem_8.py
+++ NHH_assignments/BAN401_midterm/Problem_8.py
@@ -1,4 +1,19 @@
 #Problem 8-simplified mini tax calcuater
 Tax=0.22
-standard_deduction = min(0.46 * annual_income, 92000)
 personal_allowance=100000
+def problem_8():
+    annual_income = int(input("Enter your annual income: "))
+    standard_deduction = min(0.46 * annual_income, 92000)
+    taxation= round((annual_income - standard_deduction - personal_allowance)*Tax,2)
+    if taxation <= 0:
+        taxation = 0
+    print(f"Your annual income is {annual_income} NOK")
+    print("")
+    print("=== Mini income tax calculator ===")
+    print(f"Your annual income is {annual_income} NOK")
+    print(f"Tax (22% of odinary income): {taxation} NOK")
+    print("-" * 25)
+    print(f"Tax:            {taxation} NOK")
+    print(f"Net annual income: {annual_income} NOK")
+    print(f"Net monthly income: {round(annual_income/12,2)}")
+problem_8()
